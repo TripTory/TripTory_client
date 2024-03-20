9f0d9013a175bd5ddc14d65ac6721fd29bb01d09
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-03-21 01:31:37 +0900

    Feat: add login and join router #5

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import JoinPage from "../pages/JoinPage";
+
 import React from "react";
 
 const Router = () => {
@@ -7,6 +10,8 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/join" element={<JoinPage />} />
       </Routes>
     </BrowserRouter>
   );
