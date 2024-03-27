c69b387d70d04e2f21ee27213195b942ded2dbe7
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-03-27 17:05:47 +0900

    Feat: add DiaryWritePage router #7

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,6 +3,7 @@ import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import JoinPage from "../pages/JoinPage";
 import CreateTripPage from "../pages/CreateTripPage";
+import DiaryWritePage from "../pages/DiaryWritePage";
 import React from "react";
 
 const Router = () => {
@@ -13,6 +14,7 @@ const Router = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/createTrip" element={<CreateTripPage/>} />
+        <Route path="/diary" element={<DiaryWritePage />} />
       </Routes>
     </BrowserRouter>
   );
