6d7cc2fc8271be204a306b257d8d940bf2292c9d
seo0o519 <seoyoung344@naver.com>
2024-04-03 19:44:34 +0900

    Chore: rename router #8

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,8 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import LoginPage from "../pages/LoginPage";
 import JoinPage from "../pages/JoinPage";
-import CreateTripPage from "../pages/CreateTripPage";
-import SearchPlacePage from "../pages/SearchPlacePage";
+import AddTripPage from "../pages/AddTripPage";
 import React from "react";
 
 const Router = () => {
@@ -13,8 +12,7 @@ const Router = () => {
         <Route path="/home" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
-        <Route path="/createtrip" element={<CreateTripPage/>} />
-        <Route path="/searchplace" element={<SearchPlacePage/>} />
+        <Route path="/addtrip" element={<AddTripPage/>} />
       </Routes>
     </BrowserRouter>
   );
