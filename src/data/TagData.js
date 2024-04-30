f0c0e0f68f8d5cff06d7d76157799731bf1e4327
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-04-30 16:15:38 +0900

    Fix: modify src path form #48

---
+++ src/data/TagData.js
@@ -0,0 +1,57 @@
+import beach from "../assets/images/beach.png";
+import beach1 from "../assets/images/beach1.jpeg";
+import beach2 from "../assets/images/beach2.jpeg";
+import beach3 from "../assets/images/beach3.jpeg";
+
+import mountain1 from "../assets/images/mountain1.jpeg";
+import mountain2 from "../assets/images/mountain2.jpeg";
+import mountain3 from "../assets/images/mountain3.jpeg";
+import mountain4 from "../assets/images/mountain4.jpeg";
+import mountain5 from "../assets/images/mountain5.jpeg";
+
+import dog1 from "../assets/images/dog1.jpeg";
+import dog2 from "../assets/images/dog2.jpeg";
+import dog3 from "../assets/images/dog3.jpeg";
+import dog4 from "../assets/images/dog4.jpeg";
+import dog5 from "../assets/images/dog5.jpeg";
+import dog6 from "../assets/images/dog6.jpeg";
+import dog7 from "../assets/images/dog7.jpeg";
+
+const tagData = {
+  "tags": [
+    {
+      "tagName": "바다",
+      "imagePaths": [
+        beach,
+        beach1,
+        beach2,
+        beach3
+      ]
+    },
+    {
+      "tagName": "산",
+      "imagePaths": [
+        mountain1,
+        mountain2,
+        mountain3,
+        mountain4,
+        mountain5
+      ]
+    },
+    {
+      "tagName": "강아지",
+      "imagePaths": [
+        dog1,
+        dog2,
+        dog3,
+        dog4,
+        dog5,
+        dog6,
+        dog7
+      ]
+    }
+
+  ]
+};
+
+export default tagData;
