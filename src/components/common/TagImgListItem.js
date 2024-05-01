7168b5273caf3cf08fafb79d7dea5b1cc3eea6b8
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-05-01 16:30:06 +0900

    Fix: change main photo by tag #48

--- src/components/common/TagImgListItem.js
+++ src/components/common/TagImgListItem.js
@@ -4,8 +4,14 @@ import PropTypes from "prop-types";
 import ListItem from "@mui/material/ListItem";
 import Jeju from "../../assets/images/jeju.jpg";
 import { Link } from "react-router-dom";
+import tagData from "../../data/TagData.js";
+
 
 export default function TagImgListItem({ tagName }) {
+
+  const tag = tagData.tags.find((tag) => tag.tagName === tagName);
+  const firstImagePath = tag && tag.imagePaths.length > 0 ? tag.imagePaths[0] : null;
+
   return (
     <Link to={`/tag/${tagName}`} style={{ width: "100%", height: "100%",}}>
       <StTagImgListItem>
@@ -16,7 +22,7 @@ export default function TagImgListItem({ tagName }) {
             padding: "0.2rem 0.6rem 0.2rem 0.6rem",
           }}
         >
-          <ImgDiv style={{ backgroundImage: `url(${Jeju})` }}>
+          <ImgDiv style={{ backgroundImage: `url(${firstImagePath})` }}>
             <TagP>#{tagName}</TagP>
           </ImgDiv>
         </ListItem>
