194e02d1a91ecd6bc1088c065c779d88dc08cf0f
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-05-07 16:32:34 +0900

    Feat: implement tagging logic #46

--- AI/ImageTagAnalyze.py
+++ AI/ImageTagAnalyze.py
@@ -6,8 +6,8 @@ import os
 import re
 
 from dotenv import load_dotenv
+import googletrans
 
-# load .env
 load_dotenv()
 
 subscription_key = os.environ.get('VISION_KEY')
@@ -37,7 +37,7 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 
 # Quickstart variables
 images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
-remote_image_url = "https://img4.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202304/07/kinolights/20230407081026931lbzg.jpg"
+remote_image_url = "https://gscaltexmediahub.com/wp-content/uploads/2023/05/the-day-of-ocean-2023_1.png"
 
 # tagging the image
 imageTag = []
@@ -52,4 +52,45 @@ else:
         print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
         imageTag.append(tag.name)
 
-tag = imageTag[0]
+
+tag_list = [
+    "beach",
+    "food",
+    "person",
+    "flower",
+    "mountain",
+    "sky",
+    "night",
+    "building",
+    "animal",
+    "nature",
+    "skyscraper",
+    "art",
+    "concert",
+    "tree",
+    "plant",
+    "river",
+    "cloud",
+    "car",
+    "sun"
+]
+
+if len(imageTag) % 2 == 0:
+    len_ = int(len(imageTag) / 2)
+else:
+    len_ = int(len(imageTag) / 2) + 1
+
+ret = []
+for i in range(len(imageTag[:len_])):
+    for j in range(len(tag_list)):
+        if imageTag[i] == tag_list[j]:
+            ret.append(tag_list[j])
+print(ret)
+
+translator = googletrans.Translator()
+
+tag_result = []
+for i in ret:
+    result = translator.translate(i, dest="ko")
+    tag_result.append(result.text)
+print(tag_result)
