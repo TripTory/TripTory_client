fcaaef72c3a79712f0cdee279e43f10565f957f8
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-05-14 13:53:37 +0900

    Feat: add google, kakao fetch code #66

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -7,12 +7,54 @@ import React from "react";
 
 const LoginPage = () => {
   const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+  // 요청을 보낼 함수
+  /*
+  const sendRequest = async () => {
+    try {
+      console.log(1);
+      // Fetch API를 사용하여 서버에 GET 요청을 보냅니다.
+      const response = await fetch(`${SERVER_URL}/oauth/naver`, {
+        method: "GET",
+        credentials: "include",
+        mode: "no-cors"
+      });
+
+      // 응답 데이터를 JSON 형식으로 파싱합니다.
+      const data = await response.json();
+
+      // 서버에서 받은 데이터를 처리합니다.
+      console.log(data); // 예를 들어, 서버에서 받은 응답을 콘솔에 출력하거나 다른 작업을 수행할 수 있습니다.
+    } catch (error) {
+      console.error("서버 요청 실패:", error);
+    }
+  };
+  */
+
+
 
   const goToGoogleOauth = () => {
-    window.location.href = `${SERVER_URL}/oauth/google`;
+    fetch(`${SERVER_URL}/oauth/google`, {
+      method: "GET",
+      credentials: "include",
+      mode: "no-cors"
+    })
+    .then((response) => {
+      console.log(response);
+      if (response) {
+        console.log("구글 로그인 성공!");
+        window.location.href = `${process.env.REACT_APP_SERVER_URL}/oauth/google`;
+      } else {
+        console.log("구글 로그인 실패");
+      }
+    })
+    .catch((error) => console.error("구글 로그인 에러:", error));
   };
 
   const goToNaverOauth = () => {
+
+    //sendRequest();
+
+
     fetch(`${SERVER_URL}/oauth/naver`, {
       method: "GET",
       credentials: "include",
@@ -29,9 +71,39 @@ const LoginPage = () => {
     })
     .catch((error) => console.error("네이버 로그인 에러:", error));
   };
+    ///////////
+    // fetch(`${SERVER_URL}/oauth/naver/callback`)
+    // .then((response) => {
+    //   if (!response.ok) {
+    //     throw new Error("네이버 로그인 요청에 실패했습니다.");
+    //   }
+    //   return response.json(); // JSON 형식의 응답 데이터를 파싱하여 반환
+    // })
+    // .then((data) => {
+    //   console.log(data); // 서버에서 보낸 데이터 출력
+    //   // 여기서 받은 데이터를 이용하여 클라이언트에서 필요한 작업 수행
+    // })
+    // .catch((error) => {
+    //   console.error("데이터를 가져오는 데 실패했습니다:", error);
+    // });
+  //};
 
   const goToKakaoOauth = () => {
-    window.location.href = `${SERVER_URL}/oauth/kakao`;
+    fetch(`${SERVER_URL}/oauth/kakao`, {
+      method: "GET",
+      credentials: "include",
+      mode: "no-cors"
+    })
+    .then((response) => {
+      console.log(response);
+      if (response) {
+        console.log("카카오 로그인 성공!");
+        window.location.href = `${process.env.REACT_APP_SERVER_URL}/oauth/kakao`;
+      } else {
+        console.log("카카오 로그인 실패");
+      }
+    })
+    .catch((error) => console.error("카카오 로그인 에러:", error));
   };
 
   return (
