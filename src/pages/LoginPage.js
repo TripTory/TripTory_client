c128c338ca92e16b042705b9341f1925511f0c58
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-05-14 16:41:47 +0900

    Feat: add google, kakao axios get #66

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -8,47 +8,15 @@ import axios from "axios";
 
 const LoginPage = () => {
   const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-  // 요청을 보낼 함수
-  /*
-  const sendRequest = async () => {
+
+  const goToGoogleOauth = async () => {
     try {
-      console.log(1);
-      // Fetch API를 사용하여 서버에 GET 요청을 보냅니다.
-      const response = await fetch(`${SERVER_URL}/oauth/naver`, {
-        method: "GET",
-        credentials: "include",
-        mode: "no-cors"
-      });
-
-      // 응답 데이터를 JSON 형식으로 파싱합니다.
-      const data = await response.json();
-
-      // 서버에서 받은 데이터를 처리합니다.
-      console.log(data); // 예를 들어, 서버에서 받은 응답을 콘솔에 출력하거나 다른 작업을 수행할 수 있습니다.
+      const response = await axios.get(`${SERVER_URL}/oauth/google`);
+      const { authorizationUrl } = response.data;
+      window.location.href = authorizationUrl;
     } catch (error) {
-      console.error("서버 요청 실패:", error);
+      console.error("구글 로그인 에러:", error);
     }
-  };
-  */
-
-
-
-  const goToGoogleOauth = () => {
-    fetch(`${SERVER_URL}/oauth/google`, {
-      method: "GET",
-      credentials: "include",
-      mode: "no-cors"
-    })
-    .then((response) => {
-      console.log(response);
-      if (response) {
-        console.log("구글 로그인 성공!");
-        window.location.href = `${process.env.REACT_APP_SERVER_URL}/oauth/google`;
-      } else {
-        console.log("구글 로그인 실패");
-      }
-    })
-    .catch((error) => console.error("구글 로그인 에러:", error));
   };
 
   const goToNaverOauth = async () => {
@@ -60,39 +28,15 @@ const LoginPage = () => {
       console.error("네이버 로그인 에러:", error);
     }
   };
-    ///////////
-    // fetch(`${SERVER_URL}/oauth/naver/callback`)
-    // .then((response) => {
-    //   if (!response.ok) {
-    //     throw new Error("네이버 로그인 요청에 실패했습니다.");
-    //   }
-    //   return response.json(); // JSON 형식의 응답 데이터를 파싱하여 반환
-    // })
-    // .then((data) => {
-    //   console.log(data); // 서버에서 보낸 데이터 출력
-    //   // 여기서 받은 데이터를 이용하여 클라이언트에서 필요한 작업 수행
-    // })
-    // .catch((error) => {
-    //   console.error("데이터를 가져오는 데 실패했습니다:", error);
-    // });
-  //};
 
-  const goToKakaoOauth = () => {
-    fetch(`${SERVER_URL}/oauth/kakao`, {
-      method: "GET",
-      credentials: "include",
-      mode: "no-cors"
-    })
-    .then((response) => {
-      console.log(response);
-      if (response) {
-        console.log("카카오 로그인 성공!");
-        window.location.href = `${process.env.REACT_APP_SERVER_URL}/oauth/kakao`;
-      } else {
-        console.log("카카오 로그인 실패");
-      }
-    })
-    .catch((error) => console.error("카카오 로그인 에러:", error));
+  const goToKakaoOauth = async () => {
+    try {
+      const response = await axios.get(`${SERVER_URL}/oauth/kakao`);
+      const { authorizationUrl } = response.data;
+      window.location.href = authorizationUrl;
+    } catch (error) {
+      console.error("카카오 로그인 에러:", error);
+    }
   };
 
   return (
