ae1631c47bc6f87ae14907f41875ec0639b2fb48
hummingbbird <84303489+hummingbbird@users.noreply.github.com>
2024-05-21 10:05:47 +0900

    Chore: delete unused code #70

--- src/components/common/DiaryList.js
+++ src/components/common/DiaryList.js
@@ -5,28 +5,6 @@ import axios from "axios";
 import { PropTypes } from "prop-types";
 
 export default function TripTable(props) {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-  // useEffect(() => {
-  //   let completed = false;
-
-  // eslint-disable-next-line func-style
-  //   async function get() {
-  //     const result = await axios.get(
-  //       `${process.env.REACT_APP_SERVER_URL}/travel/${props.data.}`,
-  //       { withCredentials: true },
-  //     );
-  //     if (!completed) {
-  //       setData(result.data.travels);
-  //     }
-  //     setLoading(true);
-  //   }
-  //   get();
-  //   return () => {
-  //     completed = true;
-  //   };
-  // }, []);
-
   return (
     <StTripTable>
       {props.data.map((it)=> {
